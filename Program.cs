842986d7b9e071361870392669b19fa7c8929edc
Wallstreet289 <wallstreet289@Wallstreet289s-MacBook-Pro.local>
2026-07-29 14:33:45 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("===== USER PROFILE =====");
+
+Console.Write("Enter your name: ");
+string? name = Console.ReadLine();
+
+Console.Write("Enter your age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+                  
+Console.Write("Enter your favourite programming language: ");
+string? language = Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine("===== PROFILE =====");
+Console.WriteLine($"Name     : {name}");
+Console.WriteLine($"Age      : {age}");
+Console.WriteLine($"Language : {language}");
