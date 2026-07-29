14898327b0554f02b6e08c4f62c6858a037e1c14
Wallstreet289 <wallstreet289@Wallstreet289s-MacBook-Pro.local>
2026-07-29 15:34:31 +0530

    docs:added mini project

--- Program.cs
+++ Program.cs
@@ -1,15 +1,28 @@
-﻿Console.WriteLine("===== Shopping Bill Generator =====");
-Console.Write("Product Name:");
+﻿Console.WriteLine("===== Student Grade Calculator =====");
+Console.Write("Student Name:");
 string? Name = Console.ReadLine();
-Console.Write("Quantity:");
-decimal Quantity = Convert.ToDecimal(Console.ReadLine());
-Console.Write("Price per unit: ");
-decimal PricePerUnit = Convert.ToDecimal(Console.ReadLine());
+Console.Write("Marks:");
+double Marks = Convert.ToDouble(Console.ReadLine());
 
-decimal Total = (Quantity * PricePerUnit);
-Console.WriteLine("Total:" + Total);
+if(Marks > 90)
+{
+    Console.Write("Grade: A");
+}
+else if(Marks >= 75 && Marks < 90)
+{
+    Console.Write("Grade B");
+}
+else if(Marks >= 50 && Marks < 75)
+{
+    Console.Write("Grade C");
+}
+else if(Marks >= 35 && Marks < 50)
+{
+    Console.Write("Grade D");
+}
+else
+{
+    Console.Write("Fail");
+}
 
-decimal GST = (Total + (Total * 0.18m));
 
-decimal Final_Amount = Total + GST;
-Console.Write($"Final Amount: Rs.{Final_Amount}");
